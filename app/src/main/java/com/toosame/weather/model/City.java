5cb754d799687d307b37317ffa0b24147e4f84dd
HL <326226122@qq.com>
2020-12-24 13:01:07 +0800

    Create City.java

---
+++ app/src/main/java/com/toosame/weather/model/City.java
@@ -0,0 +1,35 @@
+package com.toosame.weather.model;
+
+/**
+ * Created by 旋风小伙 on 2017年4月30日 0030.
+ */
+
+public class City {
+
+    private String name;
+
+    private int id;
+
+    public City(String name,int id){
+        this.name = name;
+        this.id = id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
